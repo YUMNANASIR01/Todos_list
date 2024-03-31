f29f1751343b081387b6775093212bccc9e12569
YUMNANASIR01 <160057966+YUMNANASIR01@users.noreply.github.com>
2024-04-01 01:41:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env/node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let answers = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What you want to add in your todos?",
+            default: "Pray, Eat, Sleep, Work, Exercise, Study, Read, Write, Listen",
+            Choices: ["Pray", "Eat", "Sleep", "Work", "Exercise", "Study", "Read", "Write", "Listen"]
+        },
+        {
+            name: "AddMore",
+            type: "confrim",
+            message: "Do you want to add more in your todos?",
+            default: "yes/No",
+            Choices: ["yes", "No"]
+        }
+    ]);
+    todos.push(answers.todo);
+    condition = answers.AddMore;
+    console.log(todos);
+}
+if (condition == false) {
+    console.log("Your todos are: ", todos);
+}
+else {
+    (condition == true);
+    console.log("Your todos are: ", todos);
+}
+if (`InRamdan == true && 
+    (todos.includes("Pray") || todos.includes("Prayer")) && 
+    (todos.includes("Eat") || todos.includes("Eating")) && 
+    (todos.includes("Sleep") || todos.includes("Sleeping")) && 
+    (todos.includes("Work") || todos.includes("Working")) && 
+    (todos.includes("Exercise") || todos.includes("Exercising")) && 
+    (todos.includes("Study") || todos.includes("Studying")) && 
+    (todos.includes("Read") || todos.includes("Reading")) && 
+    (todos.includes("Write") || todos.includes("Writing")) && 
+    (todos.includes("Listen") || todos.includes("Listening")) && Muslims fast for 30 days`) {
+    console.log("Your todos are: ", todos);
+}
